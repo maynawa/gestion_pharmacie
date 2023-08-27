ef35244f3e5d970440d46fc042a1e0eaac1fdf97
maynawa <mounaaw612@gmail.com>
2023-08-27 01:56:19 +0000

    initialize project

---
+++ src/test/java/com/example/gestion_pharmacie/GestionPharmacieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_pharmacie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPharmacieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
